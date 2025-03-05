40b5de25687084281197f57e19d9144adf7aeac3
Luiz Henrique <luizhlc02@gmail.com>
2025-03-04 22:56:18 -0300

    First commit

---
+++ src/main/java/com/api/AgroClimaWs/AgroClimaWsApplication.java
@@ -0,0 +1,13 @@
+package com.api.AgroClimaWs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroClimaWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroClimaWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/AgroClimaWs/AgroClimaWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.AgroClimaWs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroClimaWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
